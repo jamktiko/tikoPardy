fdd231fd493ff0b1fa8a90fdb5567391d0df9aba
tikexd <AG3552@student.jamk.fi>
2025-04-28 11:12:28 +0300

    modal korjaus

--- tikoPardy/src/lib/components/GameModal.svelte
+++ tikoPardy/src/lib/components/GameModal.svelte
@@ -27,31 +27,31 @@
 </div>
 
 <style>
-	.backdrop {
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100vh;
-		background: rgba(0, 0, 0, 0.75);
-		z-index: 10;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		z-index: 100;
-	}
+  .backdrop {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      background: rgba(0, 0, 0, 0.75);
+      z-index: 10;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 100;
+  }
 
-	.modal {
-		padding: 1.5rem;
-		position: fixed;
-		background-color: #F5F5F5;
-		border-radius: 0.5rem;
-		width: 90%;
-		max-width: 600px;
-		max-height: 80vh;
-		overflow-y: auto;
-		box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
-		z-index: 100;
-		color: black;
-	}
+  .modal {
+      padding: 1.5rem;
+      position: relative;  /* Muutettu fixed → relative */
+      background-color: #F5F5F5;
+      border-radius: 0.5rem;
+      width: 90%;
+      max-width: 600px;
+      max-height: 80vh;
+      overflow-y: auto;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+      z-index: 100;
+      color: black;
+  }
 </style>
